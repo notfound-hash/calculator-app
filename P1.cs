604f26f451116feb5a5d13f5e003c8957c8948a1
notfound-hash <satndolop05@gmail.com>
2025-03-11 14:12:21 +0700

    code full

--- P1.cs
+++ P1.cs
@@ -2,6 +2,23 @@ class Calculator
 {
 	static void Main (string[] args)
 	{
+		int a =10;
+		int b = 6;
 		
+		Console.WirteLine("Hasil penambahan: {0} + {1} + {2}", a,b, penambahan (a,b);
+		Console.WirteLine("Hasil penambahan: {0} + {1} + {2}", a,b, pengurangan (a,b);
+		
+		Console.Wirteline("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
+	
+	static int penambahan (int a , int b)
+	{
+		return a+b;
+	}
+	static int pengurangan (int a , int b)
+	{
+		return a-b;
+	}
+
 }
